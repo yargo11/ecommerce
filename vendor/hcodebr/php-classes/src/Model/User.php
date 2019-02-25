0463cf951ad819056e9f3199e15b4efea66e961e
Yargo Valerio <yargovale@hotmail.com>
2019-02-25 19:01:25 +1200

    tela de login

---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class User extends Model{
+	
+	const SESSION = "User";
+	
+	public static function login($login, $password){
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			
+			":LOGIN"=>$login
+		));
+		
+		if(count($results)===0)
+		{
+			throw new \Exception("Usuario ou senha invalida.");
+		}
+		
+		$data = $results[0];
+		
+		if (password_verify($password, $data["despassword"]) === true){
+			
+			$user = new User();
+			
+			$user->setData($data);
+			
+			$_SESSION[User::SESSION] = $user->getValues();
+			
+			return $user;
+		}
+		else{			
+			throw new \Exception("Usuario ou senha invalidos.");
+		}
+	}
+	
+	public static function verifyLogin($inadmin = true){
+		
+		if(
+		!isset($_SESSION[User::SESSION])
+		||
+		!$_SESSION[User::SESSION]
+		||
+		!(int)$_SESSION[User::SESSION]["iduser"]>0
+		||
+		(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+		)
+		{
+			header("Localtion: /admin/login");
+			exit;
+		}
+		
+	}
+	
+	public static function logout(){
+		
+		$_SESSION[User::SESSION] = NULL;
+	}
+}
+
+?>
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -9,6 +9,8 @@
 		private $tpl;
 		private $options = [];
 		private $defaults = [
+			"header"=>true,
+			"footer"=>true,
 			"data"=>[]
 		];
 		
@@ -29,7 +31,7 @@
 			
 			$this->setData($this->options["data"]);
 			
-			$this->tpl->draw("header");
+			if($this->options["header"] === true) $this->tpl->draw("header");
 		}
 		
 		private function setData($data = array()){
@@ -48,7 +50,7 @@
 		
 		public function __destruct(){
 			
-			$this->tpl->draw("footer");
+			if($this->options["footer"] === true)$this->tpl->draw("footer");
 		}
 	}
 
