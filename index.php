28d9d137418aaddba2f10fc527c96f553b8207dd
Yargo Valerio <yargovale@hotmail.com>
2019-02-26 14:14:31 +1200

    fatal error

--- index.php
+++ index.php
@@ -143,6 +143,36 @@ $app->post("/admin/users/:iduser", function($iduser){
 	header("Location: /admin/users");
 });
 
+$app->get("/admin/forgot", function(){
+	
+	$page = new PageAdmin([	
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot");
+});
+
+$app->post("/admin/forgot", function(){
+	
+	$user = User::getForgot($_POST["email"]);
+	
+	header("Location: /admin/forgot/sent");
+	exit;
+	
+});
+
+$app->get("adminm/forgot/sent", function(){
+	
+	$page = new PageAdmin([	
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot-sent");
+	
+});
+
 
 $app->run();
 
